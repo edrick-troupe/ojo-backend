c5dfe51e87aa7dc5113ad7bb3f8f49404a59ff8f
Etudiant O'clock <amvaero@gmail.com>
2023-07-13 16:47:17 +0200

    Seeding started in progress

--- init/deploy/structure.sql
+++ init/deploy/structure.sql
@@ -4,13 +4,18 @@ BEGIN;
 
 -- XXX Add DDLs here.
 
+CREATE DOMAIN slug AS TEXT
+  CHECK(
+    VALUE ~ '^[a-z]+(?:-[a-z]+)*$'
+);
+
 CREATE DOMAIN "email" AS text
-CHECK(
+  CHECK(
     value ~ '(?:[a-z0-9!#$%&''*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&''*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])'
 );
 
 CREATE DOMAIN "slot" AS text
-CHECK(
+  CHECK(
     value ~ '\b([01][0-9]|2[0-3]):[0-5][0-9]-([01][0-9]|2[0-3]):[0-5][0-9]\b'
 );
 
@@ -18,7 +23,7 @@ CREATE TABLE "site"
 (
     "id" INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     "name"	TEXT NOT NULL,
-    "slug" TEXT NOT NULL,
+    "slug" slug NOT NULL,
     "city" TEXT NOT NULL,
     "picture" TEXT,
     "description" TEXT,
@@ -32,7 +37,7 @@ CREATE TABLE "game"
 (
     "id" INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     "label"	TEXT NOT NULL,
-    "slug" TEXT NOT NULL,
+    "slug" slug NOT NULL,
     "picture" TEXT NOT NULL,
     "logo" TEXT,
     "description" TEXT NOT NULL,
